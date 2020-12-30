cae04f866e98c05a20599f9189772acdba4fcb04
Тестов Андрей <avtestov@ya.ru>
2020-12-30 22:30:21 +0300

    убрал лишнии HTML теги в форме добавления валюты

--- components/currency.php
+++ components/currency.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
+
 <form method="post" action="/buch/actions/addCurrency.php">
 	<div class="form-group">
 		<input type="text" class="form-control" id="name" name="name" placeholder="Название валюты - Евро / Фунт / etc">
@@ -10,5 +8,3 @@
 	</div>
 	<button type="submit" class="btn btn-success">Добавить</button>
 </form>
-</body>
-</html>

40d062dad3690790a52bf9d75b346eeb2d162fe8
Тестов Андрей <avtestov@ya.ru>
2020-12-30 22:25:46 +0300

    переписал модуль добавления операции

--- actions/addOperation.php
+++ actions/addOperation.php
@@ -1,23 +1,19 @@
 <?php
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/buch/include/functions.php";
 
-	$mysqli = new mysqli("localhost", "root", "", "bookkeeping");
-	if ($mysqli->connect_errno) {
-	    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	}
-	$writeoff = $_REQUEST['writeoff'];
-	$enrollment = $_REQUEST['enrollment'];
-	$value = $_REQUEST['value'] * 100;
-	$comment = $_REQUEST['comment'];
-	$datetime = $_REQUEST['Datetime'];
+	$writeoff = mysqli_real_escape_string(connect(), $_REQUEST['writeoff']);
+	$enrollment = mysqli_real_escape_string(connect(), $_REQUEST['enrollment']);
+	$value = mysqli_real_escape_string(connect(), $_REQUEST['value']) * 100;
+	$comment = mysqli_real_escape_string(connect(), $_REQUEST['comment']);
+	$datetime = mysqli_real_escape_string(connect(), $_REQUEST['Datetime']);
 
-	if ($result = $mysqli->query("SELECT MAX(record) as max FROM operation")) {
-		$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-		$result->close();
-		$rows = $rows[0]['max'] + 1;
+	if ($result = sql("SELECT MAX(record) as max FROM operation")) {
+		$record = $result[0]['max'] + 1;
 	}
 
-	$mysqli->query("INSERT INTO operation (debit_acc,credit_acc,value,comment, record, Datetime) VALUES ('$writeoff','$enrollment','$value','$comment','$rows', '$datetime')");
-	$mysqli->query("UPDATE accounts set value=value-'$value' where accounts.id = '$writeoff'");
-	$mysqli->query("UPDATE accounts set value=value+'$value' where accounts.id = '$enrollment'");
+	sql("INSERT INTO operation (debit_acc, credit_acc, value, comment, record, Datetime) 
+		VALUES ('$writeoff','$enrollment','$value','$comment','$record', '$datetime')");
+	sql("UPDATE accounts set value=value-'$value' where accounts.id = '$writeoff'");
+	sql("UPDATE accounts set value=value+'$value' where accounts.id = '$enrollment'");
 
-	header('Location: /buch/index.php');
+	header('Location: /buch/');

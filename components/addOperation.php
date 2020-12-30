b3be874d9fcb169be12cb26d2f231244b97d49e9
Тестов Андрей <avtestov@ya.ru>
2020-12-30 15:33:45 +0300

    Переписал запрос и вывод данных в форме добавления новой операции

--- components/addOperation.php
+++ components/addOperation.php
@@ -1,49 +1,38 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
+/*
+* Форма добавления новой расчетной операции
+*/
+$data = sql('SELECT accounts.name as account, currency.name, accounts.id FROM accounts, currency WHERE accounts.currency = currency.id');
+?>
 <form method="post" action="/buch/actions/addOperation.php">
 	<div class="form-group">
 		<input type="text" class="form-control" id="value" name="value" placeholder="555.99">
 	</div>
 	<div class="form-group">
-    <label>Счет списания</label>
-    <select class="form-control" id="writeoff" name="writeoff">
-    	<?php 
-    	if ($result = $mysqli->query("SELECT accounts.name as account, currency.name, accounts.id FROM accounts, currency WHERE accounts.currency = currency.id")) {
-	    	$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-	    	$result->close();
-		}
-		echo "<option value='0'> Внешний счет </option>";
-		foreach ($rows as $currency) {
-			$key = $currency['id'];
-			echo "<option value='$key'> " . $currency['account'] . ' - ' . $currency['name'] . '</option>';
-		}
-    	?>
-    </select>
-    <br>
-    <label>Счет зачисления</label>
-    <select class="form-control" id="enrollment" name="enrollment">
-    	<?php 
-    	if ($result = $mysqli->query("SELECT accounts.name as account, currency.name, accounts.id FROM accounts, currency WHERE accounts.currency = currency.id")) {
-	    	$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-	    	$result->close();
-		}
-		echo "<option value='0'> Внешний счет </option>";
-		foreach ($rows as $currency) {
-			$key = $currency['id'];
-			echo "<option value='$key'> " . $currency['account'] . ' - ' . $currency['name'] . '</option>';
-		}
-    	?>
-    </select>
-    <br>
+		<label>Счет списания</label>
+		<select class="form-control" id="writeoff" name="writeoff">
+		<option value='0'> Внешний счет </option>
+		<?php foreach ($data as $i): ?>
+			<option value='<?php $i['id']?>'><?= $i['account'] . ' - ' . $i['name'] ?></option>
+		<?php endforeach ?>
+		</select>
+	</div>
+	<div class="form-group">
+	    <label>Счет зачисления</label>
+	    <select class="form-control" id="enrollment" name="enrollment">
+	    	<option value='0'> Внешний счет </option>
+			<?php foreach ($data as $i): ?>
+				<option value='<?php $i['id']?>'><?= $i['account'] . ' - ' . $i['name'] ?></option>
+			<?php endforeach ?>
+	    </select>
+	</div>
     <div class="form-group">
 	    <label for="inputDate">Дата</label>
 	    <input type="date" class="form-control" value="<?php echo date('Y-m-d'); ?>" id="Datetime" name="Datetime">
   	</div>
+
     <label>Комментарий к операции</label>
-    <input type="text" class="form-control" id="comment" name="comment" placeholder="Купил цветы любимой девушке">
+    <input type="text" class="form-control" id="comment" name="comment" placeholder="Купил продукты">
     <br>
     <button type="submit" class="btn btn-success">Добавить</button>
 </form>
-</body>
-</html>
